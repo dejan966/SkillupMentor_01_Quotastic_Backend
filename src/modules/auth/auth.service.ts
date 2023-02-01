491930b28e9e106401f05293d028ae0ceed3f504
dejan966 <59951914+dejan966@users.noreply.github.com>
2023-02-01 13:48:56 +0100

    Created a function for password hashing using the bcrypt module

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,26 +1,41 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/login-user.dto';
-import { UpdateAuthDto } from './dto/register-user.dto';
+import { ConfigService } from '@nestjs/config';
+import { User } from 'src/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
+import { hash } from '../utils/bcrypt';
+import { LoginUserDto } from './dto/login-user.dto';
+import { RegisterUserDto } from './dto/register-user.dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    private usersService: UsersService,
+    //private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
+
+  async register(registerUserDto: RegisterUserDto): Promise<User> {
+    const hashedPassword: string = await hash(registerUserDto.password)
+    const user = await this.usersService.create({
+      role_id: null,
+      ...registerUserDto,
+      password: hashedPassword,
+    })
+    return user
   }
 
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  async login(userFromRequest: User, res: Response): Promise<void> {
+    const { password, ...user } = await this.usersService.findById(userFromRequest.id)
+    //const accessToken = await this.generateToken(user.id, user.email, JwtType.ACCESS_TOKEN)
+    //const accessTokenCookie = await this.generateCookie(accessToken, CookieType.ACCESS_TOKEN)
+    //const refreshToken = await this.generateToken(user.id, user.email, JwtType.REFRESH_TOKEN)
+    //const refreshTokenCookie = await this.generateCookie(refreshToken, CookieType.REFRESH_TOKEN)
+    //try {
+    //  await this.updateRtHash(user.id, refreshToken)
+    //  res.setHeader('Set-Cookie', [accessTokenCookie, refreshTokenCookie]).json({ ...user })
+    //} catch (error) {
+    //  Logging.error(error)
+    //  throw new InternalServerErrorException('Something went wrong while setting cookies into response header')
+    //}
   }
 }
