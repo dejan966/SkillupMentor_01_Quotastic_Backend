87437a4c9799138192ea01082d5f8cdedcf0c61b
dejan966 <59951914+dejan966@users.noreply.github.com>
2023-02-01 11:55:04 +0100

    All surrounding the quotes entity done

--- src/entities/quote.entity.ts
+++ src/entities/quote.entity.ts
@@ -1,1 +1,21 @@
-export class Quote {}
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { User } from './user.entity';
+
+@Entity()
+export class Quote {
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column()
+    karma:number;
+
+    @Column()
+    quote:string;
+    
+    @Column({ type: 'datetime', default: () => 'NOW()' })
+    posted_when: string
+
+    @ManyToOne(() => User, { onDelete: 'SET NULL' })
+    @JoinColumn({ name: 'user_id' })
+    user: User | null
+}
--- src/quotes/quotes.service.ts
+++ src/quotes/quotes.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Quote } from 'src/entities/quote.entity';
+import { Repository } from 'typeorm';
 import { CreateQuoteDto } from './dto/create-quote.dto';
 import { UpdateQuoteDto } from './dto/update-quote.dto';
 
 @Injectable()
 export class QuotesService {
+  constructor(
+    @InjectRepository(Quote)
+    private readonly quotesRepository: Repository<Quote>,
+  ) {}
+  
   create(createQuoteDto: CreateQuoteDto) {
     return 'This action adds a new quote';
   }
