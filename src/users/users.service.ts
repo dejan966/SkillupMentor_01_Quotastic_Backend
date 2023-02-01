738e9c0cdfbc96c5dd4eaf25b6c4492995e3e2b1
dejan966 <59951914+dejan966@users.noreply.github.com>
2023-02-01 11:36:14 +0100

    All surrounding the users entity done

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+  
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
