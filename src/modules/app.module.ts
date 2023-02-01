42b47e4c31acb45f1e8eb272f1b77c5594637fe5
dejan966 <59951914+dejan966@users.noreply.github.com>
2023-02-01 11:05:18 +0100

    More concise folder structure

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { configValidationSchema } from './schema.config';
+import { configValidationSchema } from '../config/schema.config';
 
 @Module({
   imports: [
--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { ORMConfig } from './orm.config'
+import { ORMConfig } from 'src/config/orm.config'
 
 @Module({
   imports: [
